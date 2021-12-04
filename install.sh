a279826137cd620a31da4130b2e228c5fcced595
singh-ml <dinesh.singh@riken.jp>
2021-12-05 02:27:22 +0900

    updated

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git submodule sync
 git submodule update --init --recursive --jobs 0
 export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
-python setup.py install
+python setup.py develop
 git add .
 git commit -m 'updated'
 git pull origin master
