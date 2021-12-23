1f759f3e061975c6202bb55b282508509104f46f
singh-ml <dinesh.singh@riken.jp>
2021-12-23 17:56:31 +0900

    updated

--- torch/optim/nsgd.py
+++ torch/optim/nsgd.py
@@ -139,6 +139,38 @@ class NSGD(Optimizer):
             self.Z = torch.mm(h.t(), torch.mm(U, S))
             self.Q = group['irho']**2 * torch.mm(self.Z, torch.inverse(torch.eye(rnk).to(device) + group['irho'] * torch.mm(self.Z.t(), self.Z)))
 
+    def nyscurve2(self, gradloader, model, criterion, device):
+        """Nystrom-Approximated Curvature Information"""
+        for group in self.param_groups:
+            col = group['col']
+            p = torch.cat([pi.view(-1) for pi in group['params']])
+            if col < 0:
+                col = np.int32(np.ceil(np.log2(p.shape[0])))
+            h = torch.zeros(col, p.shape[0]).to(device)
+            idx = torch.randperm(p.shape[0])[:col]
+            for batch_idx, (inputs, targets) in enumerate(gradloader):
+                inputs, targets = inputs.cuda(device), targets.cuda(device)
+                #optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                g = torch.autograd.grad(loss, group['params'], create_graph=True, retain_graph=True)
+                g = torch.cat([gi.view(-1) for gi in g])
+                for j in range(col):
+                    if j == col-1:
+                        h[j] += torch.cat([hi.reshape(-1).data for hi in torch.autograd.grad(g[idx[j]], group['params'], retain_graph=False)])
+                    else:
+                        h[j] += torch.cat([hi.reshape(-1).data for hi in torch.autograd.grad(g[idx[j]], group['params'], retain_graph=True)])
+            h = h/len(gradloader)
+            M = h[:,idx]
+            rnk = torch.matrix_rank(M)
+            U, S, V = torch.svd(M)
+            ix = range(0, rnk)
+            U = U[:, ix]
+            S = torch.sqrt(torch.diag(1./S[ix]))
+            self.Z = torch.mm(h.t(), torch.mm(U, S))
+            self.Q = group['irho']**2 * torch.mm(self.Z, torch.inverse(torch.eye(rnk).to(device) + group['irho'] * torch.mm(self.Z.t(), self.Z)))
+
+
     def nyscurve1(self, full_loss, device):
         """Nystrom-Approximated Curvature Information"""
         for group in self.param_groups:
